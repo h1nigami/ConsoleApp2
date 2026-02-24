99bde185ea1afa66c1e53325a608f5e640756b35
curtka <murkfe@gmail.com>
2026-02-24 18:59:34 +0500

    Context

--- Example.cs
+++ Example.cs
@@ -1,57 +1,45 @@
-public delegate int MathOperation(int x, int y);
-
-public class Calculator
+//Эт наш usb-port
+public interface IOperationStrategy
 {
-    public static int Add(int x, int y) => x+y;
-    public static int Substract(int x, int y) => x-y;
+    int Execute(int a, int b);
 }
 
-//void Действие без возвращаемого значения
-//Action<T>
-
-//Делегат для любого метода который возвращает значение
-//Func<int , int , int> add = (a, b) => a+b;
-
-//int sum = add(5,5);
-
-//Идеально подходит для фильтрации возвращает true/false
-//Predicate<T>
-
-// Паттерн наблюдатель 
-public class Stock
+public class AddStrategy : IOperationStrategy
 {
-    //1. Определяем делегат для обработчика событий
-    public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
-
-    //2. Обьявляем событие этого типа
-    public event PriceChangedHandler PriceChanged;
-
-    private decimal _price;
-    public decimal Price
+    public int Execute(int a, int b)
     {
-        get => _price;
-        set
-        {
-            if(_price == value) return;
-            decimal oldPrice = _price;
-            //3. Вызываем (генерируем) событие. Это можно сделать только внутри класса Stock
-            OnPriceChanged(oldPrice, _price);
-        }
+        return a + b;
     }
-    protected virtual void OnPriceChanged(decimal oldPrice, decimal newPrice)
+}
+public class SubstractStrategy : IOperationStrategy
+{
+    public int Execute(int a, int b)
     {
-        //Invoke - проверка на наличие прдписчиков и их уведомление
-        PriceChanged?.Invoke(oldPrice, newPrice);
+        return a - b;
+    }
+}
+public class MultiplyStrategy : IOperationStrategy
+{
+    public int Execute(int a, int b)
+    {
+        return a * b;
     }
 }
 
-public class Investor
+public class CalculatorContext
 {
-    private string _name;
-    public Investor(string name) {_name = name;}
-    
-    public void OnStockPriceChanged(decimal oldPrice, decimal newPrice)
+    private IOperationStrategy _strategy;
+
+    public CalculatorContext(IOperationStrategy operationStrategy)
     {
-        Console.WriteLine($"Цена изменилась с {oldPrice} на {newPrice}");
+        _strategy = operationStrategy;
+    }
+    public void SetStrategy(IOperationStrategy operationStrategy)
+    {
+        _strategy = operationStrategy;
+    }
+    public int ExecuteStrategy(int a, int b)
+    {
+        return _strategy.Execute(a, b);
     }
 }
