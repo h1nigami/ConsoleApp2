19a538216276d26f464c3becb79abeec6d870fda
curtka <murkfe@gmail.com>
2026-03-03 19:07:19 +0500

    editor

--- Example.cs
+++ Example.cs
@@ -21,7 +21,7 @@ public interface IFax
 }
 
 //Паттерн "Команда"
-class Editor
+public class Editor
 {
     public string Text {get; set;} = "";
     public void GetSelection() => Console.WriteLine("выделенный текст");
@@ -29,3 +29,38 @@ class Editor
     public void ReplaceSelectionText(string text) => Console.WriteLine($"Вставляю: {text}");
 }
 
+public abstract class Command
+{
+    protected Editor _editor;
+    private string _backup;
+
+    protected Command(Editor editor)
+    {
+        _editor = editor;
+    }
+
+    protected void SaveBAckup()
+    {
+        _backup = _editor.Text;
+    }
+
+    public void Undo()
+    {
+        _editor.Text = _backup;
+        Console.WriteLine("Отмена операции");
+    }
+
+    public abstract bool Execute():
+}
+
+public class CutCommand : Command
+{
+    public CutCommand(Editor editor):base(editor){}
+
+    public override bool Execute()
+    {
+        SaveBAckup();
+        _editor.DeleteSelectionText();
+        return true;
+    }
+}
